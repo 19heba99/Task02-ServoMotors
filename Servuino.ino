4bdcbc6e7169c3ef864e5bd5a22e910a8f7d9e1c
Heba Jamal <66881972+19heba99@users.noreply.github.com>
2020-06-28 03:38:22 +0300

    Update Servuino.ino

--- Servuino.ino
+++ Servuino.ino
@@ -3,20 +3,24 @@
 
 Servo m1;  
 Servo m2;
-int initial = 0;    
 
 void setup() {
   
   m1.attach(5); 
   m2.attach(9);
 
-  m1.write(initial);
-  m2.write(initial);
-  
-  delay(1000);
-  
   m1.write(90);
   m2.write(180);
+  
+   delay(1000);
+  
+  m1.write(180);
+  m2.write(100);
+ 
+   delay(1000);
+
+  m1.detach();
+  m2.detach();
 
 }
 
